5003d36f493d988d3ebe504fbd3ec4491a9f1ffe
Konstantin Kuznetsov <santor@RADIONET.CCS.RU>
2019-02-11 16:58:28 +0300

    Clear session script ver1

--- commands/clearsession.py
+++ commands/clearsession.py
@@ -16,8 +16,10 @@ class Command(BaseCommand):
         if account_id:
             logins = []
             utm = UTM5()
+            #Получаем список сенрвисных связок
             services = utm.get_slinks_for_account(account_id)
             if services['Result'] == 'Ok':
+                #Формируем массив логинов
                 for i in range(len(services['data'])):
                     if services['data'][i]['service_type_array'] == 3:
                         service = utm.get_ipslink_data(services['data'][i]['slink_id_array'])
@@ -30,14 +32,14 @@ class Command(BaseCommand):
                             if service['data']['login'] != '':
                                 logins.append(service['data']['login'])
                 if len((logins)) > 0:
+                    #Для всех логинов, для всех брасов формируем список комманд и выполняем их
                     from noc.sa.models.action import Action
                     from noc.sa.models.managedobject import ManagedObject
                     action = Action.objects.get(name='clearsession')
                     bras = [ManagedObject.objects.get(id=105), ManagedObject.objects.get(id=86)]
-                    commands = [[str(action.expand(bras[0],username=x))] for x in logins]
-                    pprint(commands)
-                    #[bras[0].mo.scripts.commands(commands=[]
-
+                    for i in range(len(bras)):
+                        commands = [str(action.expand(bras[i],username=x)) for x in logins]
+                        bras[i].scripts.commands(commands=commands)
         else:
             print("Need --account parametr")
 
